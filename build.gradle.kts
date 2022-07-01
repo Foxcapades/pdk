4d374f9fcb3591e3a5f97b39758c12e6037d1aa0
Elizabeth <foxcapade@gmail.com>
2022-07-01 19:04:32 -0400

    update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.7.0"
+  id("org.jetbrains.dokka") version "1.7.0"
+  `maven-publish`
+  signing
 }
 
 group = "io.foxcapades.lib"
@@ -22,3 +25,77 @@ tasks.test {
 tasks.withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "1.8"
 }
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
+
+  withJavadocJar()
+  withSourcesJar()
+}
+
+tasks.dokkaHtml {
+  outputDirectory.set(file("build/docs/dokka"))
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url = uri("https://maven.pkg.github.com/foxcapades/pdk")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
+    }
+
+    maven {
+      name = "Sonatype"
+      url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+      credentials {
+        username = project.findProperty("nexus.user") as String
+        password = project.findProperty("nexus.pass") as String
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+      pom {
+        name.set("Primitive Deques")
+        description.set("Provides deque implementations for dealing with Kotlin's primitives (without the boxing).")
+        url.set("https://github.com/foxcapades/pdk")
+
+        licenses {
+          license {
+            name.set("MIT")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("epharper")
+            name.set("Elizabeth Paige Harper")
+            email.set("foxcapades.io@gmail.com")
+            url.set("https://github.com/foxcapades")
+          }
+        }
+
+        scm {
+          connection.set("scm:git:git://github.com/foxcapades/pdk.git")
+          developerConnection.set("scm:git:ssh://github.com/foxcapades/pdk.git")
+          url.set("https://github.com/foxcapades/pdk")
+        }
+      }
+    }
+  }
+}
+
+signing {
+  useGpgCmd()
+
+  sign(configurations.archives.get())
+  sign(publishing.publications["gpr"])
+}
+
